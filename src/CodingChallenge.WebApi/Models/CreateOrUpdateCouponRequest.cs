44dea8697794e452cb69f3ba0d3c4a64d21b097d
DOMINOS\John.Schaap <john.schaap@dominos.nl>
2023-05-02 16:53:46 +0200

    Added data

--- src/CodingChallenge.WebApi/Models/CreateOrUpdateCouponRequest.cs
+++
@@ -1,3 +0,0 @@
-namespace CodingChallenge.WebApi.Models;
-
-public record CreateOrUpdateCouponRequest(string Name, string Description, string CouponCode, string[] ProductCodes, double Price, int MaxUsages, int CurrentUsages);
--- src/CodingChallenge.WebApi/Models/GetCouponResponse.cs
+++
@@ -1,3 +0,0 @@
-namespace CodingChallenge.WebApi.Models;
-
-public record GetCouponResponse(Guid Id, string Name, string Description, string CouponCode, string[] ProductCodes, double Price, int MaxUsages, int CurrentUsages);
